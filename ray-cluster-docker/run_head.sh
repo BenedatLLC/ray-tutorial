4fb17f502e43089503226803fb21cb7d31347add
Jeff Fischer <jfischer@us.fujitsu.com>
2020-11-03 20:31:15 -0800

    updates to run as master node

--- ray-cluster-docker/run_head.sh
+++ ray-cluster-docker/run_head.sh
@@ -21,5 +21,6 @@ docker build -t ray-cluster-head -f Dockerfile.head .
 #  ray-cluster-head /host/ray-cluster-docker/entry_point_head.sh
 docker run --shm-size=$SHM -d  -v `cd ..;pwd`:/host \
   -p 8265:8265 -p 6379:6379 -p 6380:6380 -p 6381:6381 -p 6382:6382 \
+  -p 10200-10700:10200-10700 \
   --name ray-head-container ray-cluster-head $REDIS_PASSWORD
 docker ps
