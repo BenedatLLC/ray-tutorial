7c2fa20f3a007a8fb54fe3b441302b2680dda5ae
Jeff Fischer <jeff.fischer@benedat.com>
2020-11-17 11:12:39 -0800

    head node changes for --network=host

--- ray-cluster-docker/run_head.sh
+++ ray-cluster-docker/run_head.sh
@@ -9,18 +9,14 @@ REDIS_PASSWORD=$1
 SHM=$2
 echo "Using shared memory size of $SHM"
 
-echo docker pull rayproject/ray
-docker pull rayproject/ray
+echo docker pull rayproject/ray:nightly
+docker pull rayproject/ray:nightly
 
 echo docker build -t ray-cluster-head -f Dockerfile.head .
 docker build -t ray-cluster-head -f Dockerfile.head .
 
 
-#docker run --shm-size=$SHM -it --rm -v `cd ..;pwd`:/host \
-#  -p 8265:8265 -p 6379:6379 -p 6380:6380 -p 6381:6381 -p 6382:6382 \
-#  ray-cluster-head /host/ray-cluster-docker/entry_point_head.sh
 docker run --shm-size=$SHM -d  -v `cd ..;pwd`:/host \
-  -p 8265:8265 -p 6379:6379 -p 6380:6380 -p 6381:6381 -p 6382:6382 \
-  -p 10200-10700:10200-10700 \
+  --network host \
   --name ray-head-container ray-cluster-head $REDIS_PASSWORD
 docker ps
--- test_nodes.py
+++ test_nodes.py
@@ -3,13 +3,38 @@
 Example from the Ray docs to get the list of nodes in the cluster
 """
 import time
+import sys
 import ray
-ray.init(address='auto')
+import ray.services
+import argparse
+
 
 @ray.remote
-def f():
-    time.sleep(0.01)
+def f(sleep_time):
+    time.sleep(sleep_time)
     return ray.services.get_node_ip_address()
 
-# Get a list of the IP addresses of the nodes that have joined the cluster.
-print(set(ray.get([f.remote() for _ in range(1000)])))
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--redis-password', default=None,
+                        help="Password to use for Redis, if non-default")
+    parser.add_argument('--address', default=None, type=str,
+                        help="Address for this Ray node")
+    parser.add_argument('--sleep-time', default=0.01, type=float,
+                        help="Time to sleep in seconds (or a fraction of a second.)"+
+                        " Defaults to 0.01 seconds.")
+    args = parser.parse_args(argv)
+
+    print("Initializing Ray...")
+    if args.redis_password is not None:
+        ray.init(address=args.address, _redis_password=args.redis_password)
+    else:
+        ray.init(address=args.address)
+
+    print("Running tasks...")
+    # Get a list of the IP addresses of the nodes that have joined the cluster.
+    print(set(ray.get([f.remote(args.sleep_time) for _ in range(1000)])))
+    print("Completed successfully.")
+
+if __name__ == '__main__':
+    sys.exit(main())
