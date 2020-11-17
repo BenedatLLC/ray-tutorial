c0963cafe5f2793f620883a821383e430039e1ff
Linda Zhou <lindazhou@gmail.com>
2020-11-17 11:11:35 -0800

    add worker files with --network=host

--- ray-cluster-docker/run_worker.sh
+++ ray-cluster-docker/run_worker.sh
@@ -10,17 +10,17 @@ REDIS_PASSWORD=$2
 SHM=$3
 echo "Using shared memory size of $SHM"
 
-echo docker pull rayproject/ray
-docker pull rayproject/ray
+echo docker pull rayproject/ray:nightly
+docker pull rayproject/ray:nightly
 
-#echo docker build -t ray-cluster-worker -f Dockerfile.worker .
-#docker build -t ray-cluster-worker -f Dockerfile.worker .
+echo docker build -t ray-cluster-worker -f Dockerfile.worker .
+docker build -t ray-cluster-worker -f Dockerfile.worker .
 
 RUN_MODE_ARGS="-d --name ray-worker-container"
+# Use this version for debugging
 #RUN_MODE_ARGS="-it --rm"
 
 docker run --shm-size=$SHM -v `cd ..;pwd`:/host $RUN_MODE_ARGS \
-  -p 6381:6381 -p 6382:6382 \
-  -p 10200-10700:10200-10700 \
+   --network host \
    ray-cluster-worker $HEAD_NODE_ADDRESS $REDIS_PASSWORD
 docker ps
